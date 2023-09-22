3907eccfe46367ebd9c203230e980deafb6825a0
AngeloSouza1 <angeloafdesouza@gmail.com>
2023-09-22 20:29:00 -0300

    chore: update code

---
+++ Exec_Ruby_Dio/require/animal.rb
@@ -0,0 +1,17 @@
+#herança
+class Animal
+    def dormir
+       puts 'zzzz'
+    end
+
+    def pular
+       puts  'toim, toim'
+    end
+end
+
+
+
+animal = Animal.new
+animal.pular
+animal.dormir
+
