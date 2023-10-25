52cb71c93808516877950cfa4a6c4cae5fdda016
AngeloSouza1 <angeloafdesouza@gmail.com>
2023-10-25 20:30:00 -0300

    feat: first project

---
+++ Projeto1/Projeto.rb
@@ -0,0 +1,18 @@
+# Solicite que o usuário insira seu nome
+print "Digite seu nome: "
+nome = gets.chomp
+
+# Solicite que o usuário insira seu sobrenome
+print "Digite seu sobrenome: "
+sobrenome = gets.chomp
+
+# Solicite que o usuário insira sua idade
+print "Digite sua idade: "
+idade = gets.chomp
+
+# Crie uma frase com as informações do usuário
+frase = "Bem-vindo, #{nome} #{sobrenome}! Você tem #{idade} anos."
+
+# Exiba a frase
+puts frase
+ p
