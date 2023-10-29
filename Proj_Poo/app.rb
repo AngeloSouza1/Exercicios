ba3d26ca2c13b4a455bf14ce0c3f07b529c690db
AngeloSouza1 <angeloafdesouza@gmail.com>
2023-10-29 14:15:17 -0300

    chore: update codes

--- Proj_Poo/app.rb
+++ Proj_Poo/app.rb
@@ -1,5 +1,5 @@
-require_relative 'produtos'
-require_relative 'loja'
+require_relative 'produto'
+require_relative 'mercado'
 
 produto = Produto.new
 produto.nome = 'Forma de bolo redonda'
@@ -9,5 +9,5 @@ produto1 = Produto.new
 produto1.nome = 'Forma de bolo cenoura'
 produto1.preco= 56.00
 
-Loja.new(produto.nome, produto.preco).comprar
-Loja.new(produto1.nome, produto1.preco).comprar
+Mercado.new(produto.nome, produto.preco).comprar
+Mercado.new(produto1.nome, produto1.preco).comprar
